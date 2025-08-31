ca7c044cb067cfb26a6d83ce76a7479fd0417d9b
Alberto Passalacqua <apcfd@outlook.com>
2025-08-31 04:12:07 -0500

    BUG: Corrected memory access issue in GQMOM implementation.
         This addresses issue #229.

--- src/quadratureMethods/momentInversion/univariate/basic/generalizedMomentInversion/generalizedMomentInversion.C
+++ src/quadratureMethods/momentInversion/univariate/basic/generalizedMomentInversion/generalizedMomentInversion.C
@@ -113,7 +113,7 @@ void Foam::generalizedMomentInversion::calcNQuadratureNodes
     // Trigger calculations of zeta_k by computing the number of realizable
     // moments.
     label nRealizableMoments = moments.nRealizableMoments(); 
-    
+
     nRegularQuadratureNodes_ 
         = (nRealizableMoments % 2 != 0) 
         ? label((nRealizableMoments - 1)/2.0) 
@@ -133,11 +133,13 @@ void Foam::generalizedMomentInversion::calcNQuadratureNodes
     abscissae_.setSize(nMaxNodes_);
     weights_.setSize(nMaxNodes_);
 
-    /*Info << "nNaxNodes = " << nMaxNodes_ << endl
-         << "nRegularQuadratureNodes = " 
-         << nRegularQuadratureNodes_ << endl
-         << "nAdditionalQuadratureNodes = " 
-         << nAdditionalQuadratureNodes_ << endl;*/
+    #ifdef FULLDEBUG
+        Info << "nMaxNodes = " << nMaxNodes_ << endl
+            << "nRegularQuadratureNodes = " 
+            << nRegularQuadratureNodes_ << endl
+            << "nAdditionalQuadratureNodes = " 
+            << nAdditionalQuadratureNodes_ << endl;
+    #endif
 }
 
 void Foam::generalizedMomentInversion::invert
@@ -188,6 +190,11 @@ void Foam::generalizedMomentInversion::correctRecurrenceR
     beta[nNodes_ - 1] = beta[nRegularQuadratureNodes_ - 1]
                        *pow(scalar(nNodes_ - 1)
                        /scalar(nRegularQuadratureNodes_ - 1), nu_);
+
+    #ifdef FULLDEBUG
+        Info << "Corrected alpha: " << alpha << endl;
+        Info << "Corrected beta: " << beta << endl;
+    #endif
 }
 
 void Foam::generalizedMomentInversion::correctRecurrenceRPlus
@@ -206,10 +213,14 @@ void Foam::generalizedMomentInversion::correctRecurrenceRPlus
 
     moments.zetas().resize(2*nMaxNodes_ - 1, 0.0);
 
+    // Take a reference to zetas and use it instead than 
+    // accessing moments.zetas() directly.
+    scalarList& zetas(moments.zetas());
+
     if (ndfTypeRPlus_ == "gamma")
     {
         const scalar m1sqr = sqr(moments(1));
-        const scalar alphaCoeff = m1sqr/(moments(0)*moments(2) - m1sqr) - 1.0; 
+        const scalar alphaCoeff = m1sqr/(moments(0)*moments(2) - m1sqr) - 1.0;
 
         for 
         (
@@ -218,19 +229,20 @@ void Foam::generalizedMomentInversion::correctRecurrenceRPlus
             i++
         )
         {
-            moments.zetas()[2*i - 1] = 
-                (i + alphaCoeff)*moments.zetas()[2*nRegularQuadratureNodes_ - 3]
+            zetas[2*i - 1] = 
+                (i + alphaCoeff)*zetas[2*nRegularQuadratureNodes_ - 3]
                /(nRegularQuadratureNodes_ - 1 + alphaCoeff);
 
-            moments.zetas()[2*i] = 
-                (i + 1)*moments.zetas()[2*nRegularQuadratureNodes_ - 2]
+            zetas[2*i] = 
+                (i + 1)*zetas[2*nRegularQuadratureNodes_ - 2]
                /(nRegularQuadratureNodes_);
 
-            // Debug info
-            //Info << "zetas[2*i-1] = " << zetas[2*i - 1] << endl;
-            //Info << "2i-1 = " << 2*i - 1 << endl;
-            //Info << "zetas[2*i] = " << zetas[2*i] << endl;
-            //Info << "2i = " << 2*i << endl;
+            #ifdef FULLDEBUG
+                Info << "zetas[2*i-1] = " << zetas[2*i - 1] << endl;
+                Info << "2i-1 = " << 2*i - 1 << endl;
+                Info << "zetas[2*i] = " << zetas[2*i] << endl;
+                Info << "2i = " << 2*i << endl;
+            #endif
         }   
     }
     else if (ndfTypeRPlus_ == "lognormal")
@@ -244,35 +256,43 @@ void Foam::generalizedMomentInversion::correctRecurrenceRPlus
             i++
         )
         {
-            moments.zetas()[2*i - 1] = 
+            zetas[2*i - 1] = 
                 pow(eta, 2*(i + 1 - nRegularQuadratureNodes_))
                *(
                     (pow(eta, 2*(i+1)) - 1.0)
                    /(pow(eta, 2*nRegularQuadratureNodes_) - 1.0)
                 )
-               *moments.zetas()[2*nRegularQuadratureNodes_ - 3];
+               *zetas[2*nRegularQuadratureNodes_ - 3];
 
-            moments.zetas()[2*i] = 
+            zetas[2*i] = 
                 pow(eta, 4*(i + 1 - nRegularQuadratureNodes_))
-               *moments.zetas()[2*nRegularQuadratureNodes_ - 2];
-            // Info << "zetas[2*i-1] = " << moments.zetas()[2*i - 1] << endl;
-            // Info << "2i-1 = " << 2*i - 1 << endl;
-            // Info << "zetas[2*i] = " << moments.zetas()[2*i] << endl;
-            // Info << "2i = " << 2*i << endl;
+               *zetas[2*nRegularQuadratureNodes_ - 2];
+
+            #ifdef FULLDEBUG
+                Info << "zetas[2*i-1] = " << zetas[2*i - 1] << endl;
+                Info << "2i-1 = " << 2*i - 1 << endl;
+                Info << "zetas[2*i] = " << zetas[2*i] << endl;
+                Info << "2i = " << 2*i << endl;
+            #endif
         }
     }    
 
-    alpha[0] = moments.zetas()[0];
+    alpha[0] = zetas[0];
 
     for (label i = 1; i < nMaxNodes_; i++)
     {
-        alpha[i] = moments.zetas()[2*i] + moments.zetas()[2*i - 1];
+        alpha[i] = zetas[2*i] + zetas[2*i - 1];
     }
 
     for (label i = 1; i < nMaxNodes_; i++)
     {
-        beta[i] = moments.zetas()[2*i - 1]*moments.zetas()[2*i - 2];
+        beta[i] = zetas[2*i - 1]*zetas[2*i - 2];
     }
+
+    #ifdef FULLDEBUG
+        Info << "Corrected alpha: " << alpha << endl;
+        Info << "Corrected beta: " << beta << endl;
+    #endif
 }
 
 void Foam::generalizedMomentInversion::correctRecurrence01
@@ -296,9 +316,12 @@ void Foam::generalizedMomentInversion::correctRecurrence01
     // we have 2*nRegularQuadratureNodes_ - 1 canonical moments
     moments.canonicalMoments().resize(2*nMaxNodes_ - 1);
 
+    scalarList& zetas(moments.zetas());
+    scalarList& canonicalMoments(moments.canonicalMoments());
+
     // We do not store p0, so canonicalMoments[0] = p1
-    scalar p1 = moments.canonicalMoments()[0]; 
-    scalar p2 = moments.canonicalMoments()[1];
+    scalar p1 = canonicalMoments[0]; 
+    scalar p2 = canonicalMoments[1];
 
     scalar alphaCoeff = (1.0 - p1 - 2*p2 + p1*p2)/p2;
     scalar betaCoeff = (p1 - p2 - p1*p2)/p2;
@@ -319,54 +342,68 @@ void Foam::generalizedMomentInversion::correctRecurrence01
         scalar pJ2i_1 = (betaCoeff + i)/(2.0*i + alphaCoeff + betaCoeff);
         scalar pJ2i = i/(2.0*i + 1.0 + alphaCoeff + betaCoeff);
 
-        if (moments.canonicalMoments()[2*nRegularQuadratureNodes_ - 3] <= pJ2n_1 
+        if (canonicalMoments[2*nRegularQuadratureNodes_ - 3] <= pJ2n_1 
          || pJ2n_1 >= pJ2i_1)
         {
-            moments.canonicalMoments()[2*i - 1] = 
-                moments.canonicalMoments()[2*nRegularQuadratureNodes_ - 3]
+            canonicalMoments[2*i - 1] = 
+                canonicalMoments[2*nRegularQuadratureNodes_ - 3]
                *pJ2i_1/pJ2n_1;
         }
         else
         {
-            moments.canonicalMoments()[2*i - 1] = 
-                (moments.canonicalMoments()[2*nRegularQuadratureNodes_ - 3]
+            canonicalMoments[2*i - 1] = 
+                (canonicalMoments[2*nRegularQuadratureNodes_ - 3]
                *(1.0 - pJ2i_1) + pJ2i_1 - pJ2n_1)/(1.0 - pJ2n_1);
         }
 
-        if (moments.canonicalMoments()[2*nRegularQuadratureNodes_ - 2] <= pJ2n
+        if (canonicalMoments[2*nRegularQuadratureNodes_ - 2] <= pJ2n
          || pJ2n >= pJ2i)
         {
-            moments.canonicalMoments()[2*i] = 
-                moments.canonicalMoments()[2*nRegularQuadratureNodes_ - 2]
+            canonicalMoments[2*i] = 
+                canonicalMoments[2*nRegularQuadratureNodes_ - 2]
                *pJ2i/pJ2n;
         }
         else
         {
-            moments.canonicalMoments()[2*i] = 
-                (moments.canonicalMoments()[2*nRegularQuadratureNodes_ - 2]
+            canonicalMoments[2*i] = 
+                (canonicalMoments[2*nRegularQuadratureNodes_ - 2]
                *(1.0 - pJ2i) + pJ2i - pJ2n)/(1.0 - pJ2n);
         }
 
-        moments.zetas()[2*i - 1] = 
-            moments.canonicalMoments()[2*i - 1]
-           *(1.0 - moments.canonicalMoments()[2*i - 2]);
+        zetas[2*i - 1] = 
+            canonicalMoments[2*i - 1]
+           *(1.0 - canonicalMoments[2*i - 2]);
 
-        moments.zetas()[2*i] = 
-            moments.canonicalMoments()[2*i]
-           *(1.0 - moments.canonicalMoments()[2*i - 1]);
+        zetas[2*i] = 
+            canonicalMoments[2*i]
+           *(1.0 - canonicalMoments[2*i - 1]);
+
+        #ifdef FULLDEBUG
+            Info << "canonicalMoments[2*i-1] = " << canonicalMoments[2*i - 1] << endl;
+            Info << "zetas[2*i-1] = " << zetas[2*i - 1] << endl;
+            Info << "2i-1 = " << 2*i - 1 << endl;
+            Info << "canonicalMoments[2*i] = " << canonicalMoments[2*i] << endl;
+            Info << "zetas[2*i] = " << zetas[2*i] << endl;
+            Info << "2i = " << 2*i << endl;
+        #endif
     }
 
-    alpha[0] = moments.zetas()[0];
+    alpha[0] = zetas[0];
 
     for (label i = 1; i < nMaxNodes_; i++)
     {
-        alpha[i] = moments.zetas()[2*i] + moments.zetas()[2*i - 1];
+        alpha[i] = zetas[2*i] + zetas[2*i - 1];
     }
 
     for (label i = 1; i < nMaxNodes_; i++)
     {
-        beta[i] = moments.zetas()[2*i - 1]*moments.zetas()[2*i - 2];
+        beta[i] = zetas[2*i - 1]*zetas[2*i - 2];
     }
+
+    #ifdef FULLDEBUG
+        Info << "Corrected alpha: " << alpha << endl;
+        Info << "Corrected beta: " << beta << endl;
+    #endif
 }
 
 // ************************************************************************* //
