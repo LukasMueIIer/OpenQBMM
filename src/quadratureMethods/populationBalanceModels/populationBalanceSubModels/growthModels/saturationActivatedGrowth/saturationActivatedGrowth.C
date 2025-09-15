96722e7cd0d4dfcda46b42545106da5dfecd3ce5
LukasMueIIer <62990034+LukasMueIIer@users.noreply.github.com>
2025-09-15 10:19:18 +0200

    Got everything to compile. Need to fix order of operations now

--- src/quadratureMethods/populationBalanceModels/populationBalanceSubModels/growthModels/saturationActivatedGrowth/saturationActivatedGrowth.C
+++ src/quadratureMethods/populationBalanceModels/populationBalanceSubModels/growthModels/saturationActivatedGrowth/saturationActivatedGrowth.C
@@ -67,7 +67,8 @@ Foam::populationBalanceSubModels::growthModels::saturationActivatedGrowth
     minAbscissa_(dict.lookupOrDefault("minAbscissa", scalar(0))),
     maxAbscissa_(dict.lookupOrDefault("maxAbscissa", GREAT)),
     lookedUpSaturation(0),
-    saturationWater_(nullptr)
+    saturationWater_(nullptr),
+    waterAbsorption_(nullptr)
 {}
 
 
@@ -91,6 +92,13 @@ Foam::populationBalanceSubModels::growthModels::saturationActivatedGrowth::lookU
             word("S_water")
         )
     );
+    
+    waterAbsorption_.reset(
+        mesh_.getObjectPtr<volScalarField>(
+            word("waterAbsoption")
+        )
+    );
+
     lookedUpSaturation = 1;
 }
 
@@ -112,5 +120,329 @@ Foam::populationBalanceSubModels::growthModels::saturationActivatedGrowth::Kg
           *neg0(abscissa - maxAbscissa_)
           *pos0((*saturationWater_)[environment] - 1); //only activate if sat>1
 }
+
+Foam::scalar
+Foam::populationBalanceSubModels::growthModels::saturationActivatedGrowth::phaseSpaceConvection
+(
+    const labelList& momentOrder,
+    const label celli,
+    const scalarQuadratureApproximation& quadrature
+)
+{
+    const PtrList<volScalarNode>& nodes = quadrature.nodes();
+    label sizeIndex = nodes[0].sizeIndex();
+
+    if(sizeIndex != -1)
+    {
+
+        //Calculate the order of the calculation
+        label sizeOrder = momentOrder[sizeIndex];
+        bool lengthBased = nodes[0].lengthBased();
+        bool volumeFraction = nodes[0].useVolumeFraction();
+
+
+
+        if (volumeFraction)
+        {
+            if (lengthBased)
+            {
+                sizeOrder += 3;
+            }
+            else
+            {
+                sizeOrder += 1;
+            }
+        }
+
+        if(
+            sizeOrder == 2
+        )
+        {
+        this->calculateSinkTerm
+        (
+            momentOrder,
+            celli,
+            quadrature
+        ); 
+        }
+    }
+
+    return growthModel::phaseSpaceConvection
+    (
+        momentOrder,
+        celli,
+        quadrature
+    );
+}
+
+
+Foam::scalar
+Foam::populationBalanceSubModels::growthModels::saturationActivatedGrowth::phaseSpaceConvection
+(
+    const labelList& momentOrder,
+    const label celli,
+    const velocityQuadratureApproximation& quadrature
+)
+{
+
+    const PtrList<volVelocityNode>& nodes = quadrature.nodes();
+    label sizeIndex = nodes[0].sizeIndex();
+
+    if(sizeIndex != -1)
+    {
+
+        //Calculate the order of the calculation
+        label sizeOrder = momentOrder[sizeIndex];
+        bool lengthBased = nodes[0].lengthBased();
+        bool volumeFraction = nodes[0].useVolumeFraction();
+
+
+
+        if (volumeFraction)
+        {
+            if (lengthBased)
+            {
+                sizeOrder += 3;
+            }
+            else
+            {
+                sizeOrder += 1;
+            }
+        }
+
+        if(
+            sizeOrder == 2
+        )
+        {
+        calculateSinkTerm
+        (
+            momentOrder,
+            celli,
+            quadrature
+        ); 
+        }
+    }
+
+    return growthModel::phaseSpaceConvection
+    (
+        momentOrder,
+        celli,
+        quadrature
+    );
+}
+
+void
+Foam::populationBalanceSubModels::growthModels::saturationActivatedGrowth::calculateSinkTerm
+(
+    const labelList& momentOrder,
+    const label celli,
+    const scalarQuadratureApproximation& quadrature
+)
+{
+    scalar gSource(0);
+
+    const PtrList<volScalarNode>& nodes = quadrature.nodes();
+    label sizeIndex = nodes[0].sizeIndex();
+
+    if (sizeIndex == -1)
+    {
+        (*waterAbsorption_)[celli] = gSource;
+    }
+
+    label sizeOrder = momentOrder[sizeIndex];
+    bool lengthBased = nodes[0].lengthBased();
+    bool volumeFraction = nodes[0].useVolumeFraction();
+
+    if (volumeFraction)
+    {
+        if (lengthBased)
+        {
+            sizeOrder += 3;
+        }
+        else
+        {
+            sizeOrder += 1;
+        }
+    }
+
+    if (sizeOrder < 1)
+    {
+        (*waterAbsorption_)[celli] = gSource;
+    }
+
+    const labelList& scalarIndexes = nodes[0].scalarIndexes();
+
+    if (!nodes[0].extended())
+    {
+        forAll(nodes, pNodeI)
+        {
+            const volScalarNode& node = nodes[pNodeI];
+
+            scalar bAbscissa =
+                max(node.primaryAbscissae()[sizeIndex][celli], scalar(0));
+
+            scalar d = node.d(celli, bAbscissa);
+
+            scalar n =
+                node.n(celli, node.primaryWeight()[celli], bAbscissa);
+
+            scalar gSourcei =
+                n
+               *Kg(d, lengthBased, celli)
+               *pow(bAbscissa, 2);
+
+            forAll(scalarIndexes, nodei)
+            {
+                if (scalarIndexes[nodei] != sizeIndex)
+                {
+                    gSourcei *=
+                        pow
+                        (
+                            node.primaryAbscissae()[nodei][celli],
+                            momentOrder[scalarIndexes[nodei]]
+                        );
+                }
+            }
+
+            gSource += gSourcei;
+        }
+
+        (*waterAbsorption_)[celli] = gSource;
+    }
+
+    forAll(nodes, pNodeI)
+    {
+        const volScalarNode& node = nodes[pNodeI];
+
+        forAll(node.secondaryWeights()[sizeIndex], sNodei)
+        {
+            scalar bAbscissa =
+                max
+                (
+                    node.secondaryAbscissae()[sizeIndex][sNodei][celli],
+                    scalar(0)
+                );
+
+            scalar d = node.d(celli, bAbscissa);
+
+            scalar n =
+                node.n(celli, node.primaryWeight()[celli], bAbscissa)
+               *node.secondaryWeights()[sizeIndex][sNodei][celli];
+
+            scalar gSourcei =
+                n
+               *Kg(d, lengthBased, celli)
+               *pow(bAbscissa, 2);
+
+            forAll(scalarIndexes, cmpt)
+            {
+                if (scalarIndexes[cmpt] != sizeIndex)
+                {
+                    gSourcei *=
+                        node.secondaryWeights()[cmpt][sNodei][celli]
+                       *pow
+                        (
+                            node.secondaryAbscissae()[cmpt][sNodei][celli],
+                            momentOrder[scalarIndexes[cmpt]]
+                        );
+                }
+            }
+
+            gSource += gSourcei;
+        }
+    }
+
+    (*waterAbsorption_)[celli] = gSource;
+}
+
+
+void
+Foam::populationBalanceSubModels::growthModels::saturationActivatedGrowth::calculateSinkTerm
+(
+    const labelList& momentOrder,
+    const label celli,
+    const velocityQuadratureApproximation& quadrature
+)
+{
+    scalar gSource(0);
+
+    const PtrList<volVelocityNode>& nodes = quadrature.nodes();
+    label sizeIndex = nodes[0].sizeIndex();
+
+    if (sizeIndex == -1)
+    {
+      (*waterAbsorption_)[celli] = gSource;
+    }
+
+    label sizeOrder = momentOrder[sizeIndex];
+    bool lengthBased = nodes[0].lengthBased();
+    bool volumeFraction = nodes[0].useVolumeFraction();
+
+    if (volumeFraction)
+    {
+        if (lengthBased)
+        {
+            sizeOrder += 3;
+        }
+        else
+        {
+            sizeOrder += 1;
+        }
+    }
+
+    if (sizeOrder < 1)
+    {
+        (*waterAbsorption_)[celli] = gSource;
+    }
+
+    const labelList& scalarIndexes = nodes[0].scalarIndexes();
+    const labelList& velocityIndexes = nodes[0].velocityIndexes();
+
+    forAll(nodes, pNodeI)
+    {
+        const volVelocityNode& node = nodes[pNodeI];
+
+        scalar bAbscissa =
+            max(node.primaryAbscissae()[sizeIndex][celli], scalar(0));
+
+        scalar d = node.d(celli, bAbscissa);
+
+        scalar n =
+            node.n(celli, node.primaryWeight()[celli], bAbscissa);
+
+        scalar gSourcei =
+            n*Kg(d, lengthBased, celli)*pow(bAbscissa, 2);
+
+        forAll(scalarIndexes, nodei)
+        {
+            if (scalarIndexes[nodei] != sizeIndex)
+            {
+                gSourcei *=
+                    pow
+                    (
+                        node.primaryAbscissae()[nodei][celli],
+                        momentOrder[scalarIndexes[nodei]]
+                    );
+            }
+        }
+
+        forAll(velocityIndexes, cmpt)
+        {
+            gSourcei *=
+                pow
+                (
+                    node.velocityAbscissae()[celli][cmpt],
+                    momentOrder[velocityIndexes[cmpt]]
+                );
+        }
+
+        gSource += gSourcei;
+    }
+
+    (*waterAbsorption_)[celli] = gSource;
+}
+
+
+
 
 // ************************************************************************* //
